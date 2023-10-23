46ea8a1475fd2202815e52b7e1584f4ddfc15d43
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2023-10-24 00:27:18 +0900

    add http server

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rust Tide
+
+非同期フレームワーク
+
+## 起動
+
+```shell
+cargo run
+
+# http://localhost:8080を起動
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+
+    app.at("/").get(|_| async { Ok("Hello, world!") });
+    app.at("/html").get(|_| async {
+        Ok(tide::Response::builder(200)
+            .body("<html><h2>Hello, tide framework!</h2></html>")
+            .header("Server", "tide")
+            .content_type(tide::http::mime::HTML)
+            .build())
+    });
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
 }
